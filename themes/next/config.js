fca601caaa6cb0cdff1a58f13495b5bc4cf4773d
uninodd <144463930+uninodd@users.noreply.github.com>
2023-09-09 00:34:05 +0800

    Update config.js

--- themes/next/config.js
+++ themes/next/config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
 
   HOME_BANNER: true, // 首页是否显示大图及标语 [true,false]
-  HOME_BANNER_Strings: ['Hi，我是一个程序员', 'Hi，我是一个打工人', 'Hi，我是一个干饭人', '欢迎来到我的博客🎉'], // 首页大图标语文字
+  HOME_BANNER_Strings: ['安安我是油切DD'], // 首页大图标语文字
 
   NAV_TYPE: 'normal', // ['fixed','autoCollapse','normal'] 分别是固定屏幕顶部并始终显示、固定屏幕顶部且滚动时隐藏，不固定屏幕顶部
 
-  POST_LIST_COVER: false, // 文章列表显示封面图
+  POST_LIST_COVER: true, // 文章列表显示封面图
   POST_LIST_PREVIEW: true, // 显示文章预览
   POST_LIST_SUMMARY: false, // 显示用户自定义摘要，有预览时优先只展示预览
 
