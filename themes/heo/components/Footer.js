c3f12338ec9ad84e1716a85f990981ffa8021467
uninodd <144463930+uninodd@users.noreply.github.com>
2023-09-09 10:45:48 +0800

    Update Footer.js

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -13,7 +13,24 @@ const Footer = ({ title }) => {
     return currentYear
   })()
 
-  return "";
+  return 
+    (
+      <footer
+            className='relative z-10 flex-shrink-0 bg-white dark:bg-[#1a191d] justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm'
+        >
+
+            {/* 颜色过度区 */}
+            <div id='color-transition' className='h-32 bg-gradient-to-b from-[#f7f9fe] to-white  dark:bg-[#1a191d] dark:from-inherit dark:to-inherit'>
+
+            </div>
+
+            {/* 社交按钮 */}
+            <div className='w-full h-24'>
+                <SocialButton />
+            </div>
+        </div>
+        </footer >
+                  )
 }
 
 export default Footer
