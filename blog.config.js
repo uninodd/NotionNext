1ac8d0ab9772282b09d5cdd71360093588bf1325
uninodd <144463930+uninodd@users.noreply.github.com>
2023-09-09 13:35:03 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -18,8 +18,8 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '油切DD', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '油切DD', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://uninodd.com', // 网站地址
+  BIO: process.env.NEXT_PUBLIC_BIO || '會剪輯的柴犬', // 作者简介
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://notion-next-six-dusky-99.vercel.app/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'uninodd, 油切DD', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
