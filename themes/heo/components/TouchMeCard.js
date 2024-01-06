bdc45854d5b1c038f265bdf51af5c7851c1b08c3
uninodd <144463930+uninodd@users.noreply.github.com>
2024-01-06 11:29:42 +0800

    Update TouchMeCard.js

--- themes/heo/components/TouchMeCard.js
+++ themes/heo/components/TouchMeCard.js
@@ -20,7 +20,7 @@ export default function TouchMeCard() {
                     <div className='h-full'>
                         <h2 className='font-[1000] text-3xl'>{CONFIG.SOCIAL_CARD_TITLE_1}</h2>
                         <h3 className='pt-2'>{CONFIG.SOCIAL_CARD_TITLE_2}</h3>
-                        <div className='absolute left-0 top-0 w-full h-full' style={{ background: 'url(https://file.notion.so/f/f/488425be-6b65-4c3c-a100-e125475177d9/e796ca12-773e-4d83-b67a-fb2a90c5485f/DC%E7%BE%A4.png?id=45233bc0-fa1c-43bb-8cd9-e9e57390930e&table=block&spaceId=488425be-6b65-4c3c-a100-e125475177d9&expirationTimestamp=1694325600000&signature=xkLWbmZkq3XYaZ-LTQlHTol0QfwQhA_MSvFoagjp7yM&downloadName=DC%E7%BE%A4.png) center center no-repeat' }}></div>
+                        <div className='absolute left-0 top-0 w-full h-full' style={{ background: 'url(https://uninodd.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F488425be-6b65-4c3c-a100-e125475177d9%2Fe796ca12-773e-4d83-b67a-fb2a90c5485f%2FDC%25E7%25BE%25A4.png?table=block&id=45233bc0-fa1c-43bb-8cd9-e9e57390930e&spaceId=488425be-6b65-4c3c-a100-e125475177d9&width=570&userId=&cache=v2) center center no-repeat' }}></div>
                     </div>}
                 backContent={<Link href={CONFIG.SOCIAL_CARD_URL}>
                     <div className='font-[1000] text-xl h-full'>
